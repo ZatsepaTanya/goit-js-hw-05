d2e63e3d9a44cebe54c21b666305b0387fd1c9b9
ZatsepaTanya <Tanyamath@i.ua>
2021-01-12 15:07:10 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@
 //   добавь метод getInfo(), который выводит в консоль значения полей
 //    login и email объекта который его вызвал.
 
-const Account = function(login, email){
+const Account = function({login, email}){
 this.login = login;
 this.email = email;
 };
@@ -11,10 +11,16 @@ Account.prototype.getInfo = function(){
     console.log (`Login: ${this.login}, Email: ${this.email}`);
 };
 
-const mango = new Account('Mangozedog', 'mango@dog.woof');
-
-mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-
-const poly = new Account('Poly','poly@mail.com');
-
-poly.getInfo(); // Login: Poly, Email: poly@mail.com
+const mango = new Account({
+    login: 'Mangozedog',
+    email: 'mango@dog.woof',
+  });
+  
+  mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+  
+  const poly = new Account({
+    login: 'Poly',
+    email: 'poly@mail.com',
+  });
+  
+  poly.getInfo();// Login: Poly, Email: poly@mail.com
